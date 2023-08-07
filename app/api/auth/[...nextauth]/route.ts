f52f906c540a93d51db6db9efdaf53db04eb163f
Zakaria Arr <zarr@Zakarias-MacBook-Pro.local>
2023-08-07 10:12:05 +0100

    first commit

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // This is where you need to retrieve user data
+        // to verify with credentials
+        // Docs: https://next-auth.js.org/configuration/providers/credentials
+        const user = { id: "42", name: "Dave", password: "nextauth" };
+        if (credentials?.username === user.name && credentials?.password === user.password) {
+          return user
+        } else {
+          return null
+        }
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import Navbar from '@/components/navbar';
 import './globals.css'
+import AuthProvider from './AuthProvider';
 
 export default function RootLayout({
   children,
@@ -6,13 +8,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+    <AuthProvider>
+      <html lang="en">
+        {/*
+          <head /> will contain the components returned by the nearest parent
+          head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+        */}
+        <head />
+        <body>
+          <Navbar />
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
+  );
 }
